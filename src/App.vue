e235aa0dd39530e7d05782d931ef1d97d16c6c0a
rary <xogml6915@gmail.com>
2022-12-27 23:39:11 +0900

    vue-advanced - day1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="App">
+    hello
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    fetchData(){
+      console.log("hello");
+    }
   }
 }
 </script>
